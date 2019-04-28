cfb700fb265d3a316c07cdf7ed921754e3f19d60
thomaskisner <tjkisner@gmail.com>
2019-04-28 10:08:39 -0400

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>hi</div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
